3967d975c31c5bd960115a2d65ea5c24f99f9e6c
GanguChirudeep <144471481+GanguChirudeep@users.noreply.github.com>
2023-09-16 22:32:58 +0530

    Add files via upload

---
+++ Lab4-ML (1).py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+# Define the data
+data = {
+    'age': ['<=30', '<=30', '31…40', '>40', '>40', '>40', '31…40', '<=30', '<=30', '>40', '<=30', '31…40', '31…40', '>40'],
+    'income': ['high', 'high', 'high', 'medium', 'low', 'low', 'low', 'medium', 'low', 'medium', 'medium', 'medium', 'high', 'medium'],
+    'student': ['no', 'no', 'no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'no'],
+    'credit_rating': ['fair', 'excellent', 'fair', 'fair', 'fair', 'excellent', 'excellent', 'fair', 'fair', 'fair', 'excellent', 'excellent', 'fair', 'excellent'],
+    'buys_computer': ['no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'yes', 'yes', 'yes', 'no']
+}
+
+# Create a DataFrame from the data
+df = pd.DataFrame(data)
+
+# Display the dataset
+print(df)
+
+
+# In[2]:
+
+
+import pandas as pd
+from math import log2
+
+# Define the dataset
+data = pd.DataFrame({
+    'age': ['<=30', '<=30', '31-40', '>40', '>40', '>40', '31-40', '<=30', '<=30', '>40', '<=30', '31-40', '31-40', '>40'],
+    'income': ['high', 'high', 'high', 'medium', 'low', 'low', 'low', 'medium', 'low', 'medium', 'medium', 'medium', 'high', 'medium'],
+    'student': ['no', 'no', 'no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'yes', 'yes', 'yes', 'yes', 'no'],
+    'credit_rating': ['fair', 'excellent', 'fair', 'fair', 'fair', 'excellent', 'excellent', 'fair', 'fair', 'fair', 'excellent', 'excellent', 'fair', 'fair'],
+    'buys_computer': ['no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'yes', 'yes', 'yes', 'no']
+})
+
+# Calculate the entropy for the 'buys_computer' feature
+def calculate_entropy(data):
+    class_counts = data.value_counts(normalize=True)
+    entropy = -sum(p * log2(p) for p in class_counts)
+    return entropy
+
+entropy_buys_computer = calculate_entropy(data['buys_computer'])
+print(f'Entropy of "buys_computer": {entropy_buys_computer:.4f}')
+
+# Calculate the entropy for each feature
+feature_entropies = {}
+for feature in data.columns[:-1]:  # Exclude the target variable 'buys_computer'
+    unique_values = data[feature].unique()
+    weighted_entropy = 0
+    for value in unique_values:
+        subset = data[data[feature] == value]
+        entropy = calculate_entropy(subset['buys_computer'])
+        weight = len(subset) / len(data)
+        weighted_entropy += weight * entropy
+    feature_entropies[feature] = weighted_entropy
+
+# Print the entropy for each feature
+print('\nEntropy for each feature at the root node:')
+for feature, entropy in feature_entropies.items():
+    print(f'{feature}: {entropy:.4f}')
+
+
+# In[4]:
+
+
+import pandas as pd
+import math
+
+# Load the dataset
+data = {
+    'age': ['<=30', '<=30', '31…40', '>40', '>40', '>40', '31…40', '<=30', '<=30', '>40', '<=30', '31…40', '31…40', '>40'],
+    'income': ['high', 'high', 'high', 'medium', 'low', 'low', 'low', 'medium', 'low', 'medium', 'medium', 'medium', 'high', 'medium'],
+    'student': ['no', 'no', 'no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'no'],
+    'credit_rating': ['fair', 'excellent', 'fair', 'fair', 'fair', 'excellent', 'excellent', 'fair', 'fair', 'fair', 'excellent', 'excellent', 'fair', 'excellent'],
+    'buys_computer': ['no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'yes', 'yes', 'yes', 'no']
+}
+
+df = pd.DataFrame(data)
+
+# Calculate the entropy of the target variable 'buys_computer' at the root node
+total_samples = len(df)
+yes_count = len(df[df['buys_computer'] == 'yes'])
+no_count = len(df[df['buys_computer'] == 'no'])
+
+entropy_root = 0
+
+if yes_count > 0:
+    p_yes = yes_count / total_samples
+    entropy_root -= p_yes * math.log2(p_yes)
+    
+if no_count > 0:
+    p_no = no_count / total_samples
+    entropy_root -= p_no * math.log2(p_no)
+
+# Calculate Information Gain for each feature
+def calculate_entropy(attribute):
+    entropy = 0
+    attribute_counts = df.groupby(attribute)['buys_computer'].value_counts()
+    total_samples = len(df)
+    
+    for value, count in attribute_counts.items():
+        value_samples = len(df[df[attribute] == value[0]])
+        p_value = value_samples / total_samples
+        p_class = count / value_samples
+        entropy -= p_value * p_class * math.log2(p_class)
+    
+    return entropy
+
+information_gain = {}
+for feature in df.columns[:-1]:  # Exclude the target variable
+    entropy_feature = calculate_entropy(feature)
+    information_gain[feature] = entropy_root - entropy_feature
+
+# Identify the feature with the highest Information Gain as the root node
+root_node = max(information_gain, key=information_gain.get)
+
+print("Entropy at root node:", entropy_root)
+
+print("\nInformation Gain for each feature:")
+for feature, ig in information_gain.items():
+    print(f"{feature}: {ig}")
+
+print("The first feature to select for the decision tree:", root_node)
+
+
+# In[5]:
+
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import OneHotEncoder
+
+# Load the dataset
+data = {
+    'age': ['<=30', '<=30', '31…40', '>40', '>40', '>40', '31…40', '<=30', '<=30', '>40', '<=30', '31…40', '31…40', '>40'],
+    'income': ['high', 'high', 'high', 'medium', 'low', 'low', 'low', 'medium', 'low', 'medium', 'medium', 'medium', 'high', 'medium'],
+    'student': ['no', 'no', 'no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'no'],
+    'credit_rating': ['fair', 'excellent', 'fair', 'fair', 'fair', 'excellent', 'excellent', 'fair', 'fair', 'fair', 'excellent', 'excellent', 'fair', 'excellent'],
+    'buys_computer': ['no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'yes', 'yes', 'yes', 'no']
+}
+
+df = pd.DataFrame(data)
+
+# Separate the features (X) and the target variable (y)
+X = df.drop('buys_computer', axis=1)
+y = df['buys_computer']
+
+# Perform one-hot encoding on the categorical features
+X_encoded = pd.get_dummies(X, columns=['age', 'income', 'student', 'credit_rating'])
+
+# Create and train the Decision Tree model
+model = DecisionTreeClassifier()
+model.fit(X_encoded, y)
+
+# Get the training set accuracy
+training_accuracy = model.score(X_encoded, y)
+print("Training Set Accuracy:", training_accuracy)
+
+# Get the depth of the constructed tree
+tree_depth = model.get_depth()
+print("Tree Depth:", tree_depth)
+
+
+# In[6]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn import tree
+
+# Define the dataset as you provided
+data = {
+    'age': ['<=30', '<=30', '31…40', '>40', '>40', '>40', '31…40', '<=30', '<=30', '>40', '<=30', '31…40', '31…40', '>40'],
+    'income': ['high', 'high', 'high', 'medium', 'low', 'low', 'low', 'medium', 'low', 'medium', 'medium', 'medium', 'high', 'medium'],
+    'student': ['no', 'no', 'no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'no'],
+    'credit_rating': ['fair', 'excellent', 'fair', 'fair', 'fair', 'excellent', 'excellent', 'fair', 'fair', 'fair', 'excellent', 'excellent', 'fair', 'excellent'],
+    'buys_computer': ['no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'yes', 'yes', 'yes', 'no']
+}
+
+df = pd.DataFrame(data)
+
+# Encode categorical features
+le = LabelEncoder()
+for col in df.columns:
+    df[col] = le.fit_transform(df[col])
+
+# Define features and target variable
+X = df.drop('buys_computer', axis=1)
+y = df['buys_computer']
+
+# Create and fit the decision tree model
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+# Visualize the decision tree
+plt.figure(figsize=(20, 10))
+tree.plot_tree(model, filled=True, feature_names=list(df.columns[:-1]), class_names=['No', 'Yes'])
+plt.show()
+
+
+# In[ ]:
+
+
+
+
